5132807ea96da0b4816ada636b883d998624c33b
hoperAleksei <85HopER85@gmail.com>
2020-10-09 20:13:35 +1000

    Init clion environment

---
+++ cmake-build-debug/CMakeFiles/HW_Target.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW_Target.dir/main.cpp.o"
+  "HW_Target"
+  "HW_Target.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW_Target.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/HW_Target.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for HW_Target.
+# This may be replaced when dependencies are built.
