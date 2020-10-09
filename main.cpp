0efddc50931c6830bf48687ecbc4ae34ddb98a98
hoperAleksei <85HopER85@gmail.com>
2020-10-09 22:03:31 +1000

    Homework is done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,54 @@
 #include <iostream>
+#include <random>
+#include <cmath>
+
+using namespace std;
+
+float randCoord(float coord)
+{
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_real_distribution<> dist(-5, 5);
+	return  coord + float(dist(gen));
+}
+
+float getShotRad()
+{
+	float x, y;
+	cout << "Ведите координату x выстрела: ";
+	cin >> x;
+	cout << "Ведите координату y выстрела: ";
+	cin >> y;
+
+	x = randCoord(x);
+	y = randCoord(y);
+
+	cout << "Вы попали в точку: (" << x << "; " << y << ")" << endl;
+
+	return sqrt(x*x + y*y);
+}
+
+int getShotScore(float Rad)
+{
+	int res = 5 - floor(Rad);
+	return res > 0 ? res : 0;
+}
+
+void game()
+{
+	int resScore = 0;
+	int score;
+	
+	for (int i = 0; i < 5; i++)
+	{
+		score = getShotScore(getShotRad());
+		cout << "За этот выстрел вы получили " << score << " очков!" << endl;
+		resScore += score;
+	}
+	cout << "По итогу вы получили " << resScore << " очков" << (resScore < 10 ? ", поэтому вы ЛУЗЕР!" : ".") << endl;
+}
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	game();
 	return 0;
 }
